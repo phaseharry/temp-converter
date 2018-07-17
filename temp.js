95ca5d351c1bbdd182801053dd3753179473f634
Harry C <harrychenhc@icloud.com>
2018-07-17 19:10:19 -0400

    updated

--- temp.js
+++ temp.js
@@ -1,3 +1,15 @@
+/*
+const tempConverter = temp => {
+  temp = temp.toLowerCase();
+  let num = temp.split(' ')[0] * 1; //assumes entered format of argument is 'number fahrenheit'
+  if (temp.search('fahrenheit') >= 0) return convertF2C(num);
+  if (temp.search('celsius') >= 0) return convertC2(num);
+};
+
+tempConverter takes in a string with a number and fahrenheit or celsius
+ex `30 celsius`;
+and calls either convertF2C or convertC2F;
+*/
 const convertF2C = temp => {
   return (temp - 32) * (5 / 9); //+ `Celcius`;
 };
